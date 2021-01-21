9b901fd34d682f24bf5820447979880083758bae
wullieTheXeroxCryptoDev <77131727+wullieTheXeroxCryptoDev@users.noreply.github.com>
2021-01-21 16:49:33 +0000

    Add files via upload

---
+++ java tcp/udncp soket/src/com/william/socket/server/Main.java
@@ -0,0 +1,32 @@
+package com.william.socket.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void ip(){
+        String privetIp ;
+        String publicIp;
+        int Port;
+
+    }
+
+    public static void tcpHandShake(){
+        // codes each client exchanges for connection
+        //unencrypted
+        String clientPrivet1 = "thisIsTheCorrectCode";
+        String clientPrivet2 = "";
+        String clientPrivet3 = "";
+        String clientPrivet4 = "";
+
+        //encrypted
+        String clientPublic1  = "1A7B56781EF";
+
+    }
+}
+
+
